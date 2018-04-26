ff6686775932aaf095efe6590da34037f4005dc5
toffyjs <32435307+toffyjs@users.noreply.github.com>
2018-04-26 21:40:18 +0300

    my commit message

--- js/app.js
+++ js/app.js
@@ -2,12 +2,12 @@
 
 var Tag = {
   jsInline: function(src, callback) {
-    var script = document.createElement("script");
+    var script = document.createElement('script');
     script.async = false;
-    script.type = "text/javascript";
-    const d = "data:text/javascript;base64," + btoa(src);
+    script.type = 'text/javascript';
+    const d = 'data:text/javascript;base64,' + btoa(src);
     script.src = d;
-    document.getElementsByTagName("head")[0].appendChild(script);
+    document.getElementsByTagName('head')[0].appendChild(script);
     let loaded;
     if (callback) {
       script.onreadystatechange = script.onload = function() {
@@ -16,19 +16,19 @@ var Tag = {
     }
   },
   css: function(src, callback, onerror) {
-    var tag = document.createElement("link"),
+    var tag = document.createElement('link'),
       loaded;
-    tag.setAttribute("href", src);
-    tag.rel = "stylesheet";
-    document.getElementsByTagName("head")[0].appendChild(tag);
+    tag.setAttribute('href', src);
+    tag.rel = 'stylesheet';
+    document.getElementsByTagName('head')[0].appendChild(tag);
   },
   js: function(src, callback) {
-    var script = document.createElement("script"),
+    var script = document.createElement('script'),
       loaded;
-    script.type = "text/javascript";
-    script.setAttribute("src", src);
+    script.type = 'text/javascript';
+    script.setAttribute('src', src);
     script.async = false;
-    document.getElementsByTagName("head")[0].appendChild(script);
+    document.getElementsByTagName('head')[0].appendChild(script);
     if (callback) {
       script.onreadystatechange = script.onload = function() {
         if (!loaded) {
@@ -47,14 +47,14 @@ function updateTag(type, name, id) {
     Tag[type](
       name,
       () => {
-        window.openFromView("extranal", [name], Klasses);
+        window.openFromView('external', [name], Klasses);
       },
       e => {
-        window.openFromView("extranalerr", [name], Klasses);
+        window.openFromView('external_err', [name], Klasses);
       }
     );
   } else {
-    tag[type == "js" ? "src" : "link"] = name;
+    tag[type == 'js' ? 'src' : 'link'] = name;
   }
 }
 
@@ -69,7 +69,7 @@ function updateCode(code) {
                         console.log(e.stack);  
                     }
             `;
-  var script = document.createElement("script");
+  var script = document.createElement('script');
   script.innerHTML = scr;
   try {
     document.head.appendChild(script);
@@ -77,30 +77,25 @@ function updateCode(code) {
 }
 
 function run(name, propsstr, style) {
-  const appdiv = document.getElementById("app");
+  const appdiv = document.getElementById('app');
   style = style || {
     opacity: 0.3
   };
-  ReactDOM.render(React.createElement("div"), appdiv);
-  eval("props=" + (propsstr || "{}"));
+  ReactDOM.render(React.createElement('div'), appdiv);
+  eval('props=' + (propsstr || '{}'));
   try {
-    ReactDOM.render(
-      React.createElement(
-        "div",
-        { style },
-        React.createElement(Klasses[name], props)
-      ),
-      appdiv
-    );
+    window.app = React.createElement(Klasses[name], props);
+    window.parent = React.createElement('div', { style }, window.app);
+    window.parentr = ReactDOM.render(window.parent, appdiv);
   } catch (e) {
     console.error(e);
   }
 }
 
 function updateStyle(css, code) {
-  css = "style" + css;
+  css = 'style' + css;
   if (!this[css]) {
-    var script = document.createElement("style");
+    var script = document.createElement('style');
     script.id = css;
     document.head.appendChild(script);
   }
@@ -120,16 +115,15 @@ function updateMethods(name, obj) {
   }
   for (var k in obj) {
     try {
-      if (k === "render") Klasses[name].prototype.render = Function(obj[k]);
+      if (k === 'render') Klasses[name].prototype.render = Function(obj[k]);
       else {
         if (obj[k] === -1) {
           delete Klasses[name].prototype[k];
-        } else if (obj[k] !== 0)
-          Klasses[name].prototype[k] = Function.apply(Function, obj[k]);
+        } else if (obj[k] !== 0) Klasses[name].prototype[k] = Function.apply(Function, obj[k]);
       }
     } catch (e) {
       console.error(k, e, obj[k]);
-      window.openFromView && window.openFromView("log", [e.message]);
+      window.openFromView && window.openFromView('log', [e.message]);
     }
   }
 }
@@ -138,9 +132,9 @@ const Klasses = {};
 
 //=========================================================================================================
 function updateSelect(nodeid) {
-  var sides = ["Left", "Right", "Bottom", "Top"];
+  var sides = ['Left', 'Right', 'Bottom', 'Top'];
   if (!nodeid) {
-    if (inspector) inspector.style.display = "none";
+    if (inspector) inspector.style.display = 'none';
     if (window.elSelected) {
       window.elSelected.style.outline = null;
       window.elSelected = null;
@@ -162,39 +156,50 @@ function updateSelect(nodeid) {
   var l = r.left;
   if (inspector) {
     for (var o of sides) {
-      var p = parseFloat(cs["padding" + o]);
-      var b = parseFloat(cs["border" + o + "Width"]);
-      inspector.style["border" + o + "Width"] = p + "px";
-      if (o === "Top" || o === "Bottom") {
+      var p = parseFloat(cs['padding' + o]);
+      var b = parseFloat(cs['border' + o + 'Width']);
+      inspector.style['border' + o + 'Width'] = p + 'px';
+      if (o === 'Top' || o === 'Bottom') {
         h -= p + b;
-        if (o === "Top") t += b;
+        if (o === 'Top') t += b;
       } else {
-        if (o === "Left") l += b;
+        if (o === 'Left') l += b;
         w -= p + b;
       }
     }
-    inspector.style.width = w + "px";
-    inspector.style.height = h + "px";
-    inspector.style.top = t + "px";
-    inspector.style.left = l + "px";
-    inspector.style.display = "block";
+    inspector.style.width = w + 'px';
+    inspector.style.height = h + 'px';
+    inspector.style.top = t + 'px';
+    inspector.style.left = l + 'px';
+    inspector.style.display = 'block';
   }
-  el.style.outline = "1px solid #0091cc";
+  el.style.outline = '1px solid #0091cc';
   window.elSelected = el;
 }
 
 function $print() {
-  window.openFromView && window.openFromView("log", arguments, Klasses);
+  window.openFromView && window.openFromView('log', arguments, Klasses);
 }
 
 let oldHTMLFocus = HTMLElement.prototype.focus;
 HTMLElement.prototype.focus = function() {
-  console.log("try to focus!!!", window.canFocus);
+  console.log('try to focus!!!', window.canFocus);
   if (!window.canFocus) return;
   oldHTMLFocus.apply(this, arguments);
 };
 
-console.log("App ready!!!");
+console.log('App ready!!!');
 
-var inspector = document.getElementById("inspector");
+var inspector = document.getElementById('inspector');
 //})()
+
+window.findReactComponent = function(el) {
+  for (const key in el) {
+    if (key.startsWith('__reactInternalInstance$')) {
+      const fiberNode = el[key];
+
+      return fiberNode && fiberNode.return && fiberNode.return.stateNode;
+    }
+  }
+  return null;
+};
